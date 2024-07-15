9c95817c2606b7b913ddd9a1d78d50b3ec0719f5
Kevin0532 <144108414+Kevin0532@users.noreply.github.com>
2024-07-15 19:18:20 +0200

    Login Styling update

--- login.php
+++ login.php
@@ -41,21 +41,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="styling.css">
 </head>
 
 <body>
     <div class="container">
-        <h1>Login</h1>
+        <h1 class="loginContainer">Login</h1>
 
         <?php if ($is_invalid) : ?>
             <em>Invalid Login</em>
         <?php endif; ?>
-
         <form method="post">
             <div>
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                <input type="email" name="email" id="email" value="<?=htmlspecialchars($_POST["email"] ?? "") ?>">
             </div>
 
             <div>
@@ -68,7 +67,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             </div>
         </form>
 
-        <p>Geen account? <a href="signup.html">Register hier.</a></p>
+        <div class="registerLabel">
+            <p>Nog geen account? <a style="text-decoration: none; color: #07f; font-weight: 500;" href="signup.html"> Aanmelden</a></p>
+        </div>
+
+
     </div>
 </body>
 
